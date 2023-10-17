5029e450cb62c10dacd0e473baafc7c1fcf858d9
cameroncaen <cameron.caen@wsu.edu>
2023-10-17 16:01:29 -0700

    Added model class without relationship.

--- TutorProject/Apps/TutorApp/models.py
+++ TutorProject/Apps/TutorApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Class(models.Model):
+    id = models.IntegerField(primary_key=True)
+    coursenum = models.CharField(max_length=3)
+    title = models.CharField(max_length=100)
+    major = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.title
