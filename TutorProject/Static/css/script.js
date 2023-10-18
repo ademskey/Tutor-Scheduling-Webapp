1113e2ec27d36c9bca016b0a02e5c13687a2a952
Matthew Pham <matthewpham123@outlook.com>
2023-10-18 14:34:41 -0700

    Fully implemeted the Base.html

--- TutorProject/Static/css/script.js
+++ TutorProject/Static/css/script.js
@@ -1,15 +1,36 @@
 document.querySelector('#closeSidebarBtn').addEventListener('click', function() {
     var sidebar = document.querySelector('#sidebar');
+    var closeBtn = document.querySelector('#closeSidebarBtn');
+    var openBtn = document.querySelector('#openSidebarBtn');
+    var wrapper = document.querySelector(".wrapper");
+
+    // Toggle the collapsed class on wrapper
+    wrapper.classList.toggle("collapsed");
+
+    // Toggle the closed class on sidebar
+    sidebar.classList.toggle('closed');
+
     if (sidebar.classList.contains('closed')) {
-        sidebar.style.width = '250px';
-        sidebar.classList.remove('closed');
+        sidebar.style.width = '50px';  // Closed width
+        closeBtn.style.display = 'none';  // Hide close button
+        openBtn.style.display = 'block';  // Show menu button
     } else {
-        sidebar.style.width = '50px';  // This value can be adjusted for desired closed width
-        sidebar.classList.add('closed');
+        sidebar.style.width = '250px';  // Opened width
+        closeBtn.style.display = 'block';  // Show close button
+        openBtn.style.display = 'none';  // Hide menu button
     }
 });
 
-document.getElementById("closeSidebarBtn").addEventListener("click", function() {
+// Event listener to open the sidebar when the menu button is clicked
+document.querySelector('#openSidebarBtn').addEventListener('click', function() {
+    var sidebar = document.querySelector('#sidebar');
+    var closeBtn = document.querySelector('#closeSidebarBtn');
+    var openBtn = document.querySelector('#openSidebarBtn');
     var wrapper = document.querySelector(".wrapper");
-    wrapper.classList.toggle("collapsed");
+
+    wrapper.classList.remove("collapsed");
+    sidebar.classList.remove('closed');
+    sidebar.style.width = '250px';  // Opened width
+    closeBtn.style.display = 'block';  // Show close button
+    openBtn.style.display = 'none';  // Hide menu button
 });
