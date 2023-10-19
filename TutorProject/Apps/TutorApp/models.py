ae427642244a1ecebde6fed66ccc0a686e535dff
Trenton Bergdahl <trenton.bergdahl@wsu.edu>
2023-10-18 20:56:58 -0700

    Models!

--- TutorProject/Apps/TutorApp/models.py
+++ TutorProject/Apps/TutorApp/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class Class(models.Model):
-    id = models.IntegerField(primary_key=True)
     coursenum = models.CharField(max_length=3)
     title = models.CharField(max_length=100)
     major = models.CharField(max_length=20)
@@ -12,18 +11,21 @@ class Class(models.Model):
 
 
 class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(("email address"), unique = True)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
     is_admin = models.BooleanField(default=False)
-    is_student = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=True)
     is_tutor = models.BooleanField(default=False)
 
 class Admin(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, default=CustomUser.objects.get(username='admin_default'))
-
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, null=True)
+    pass
 class Tutor(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, default=CustomUser.objects.get(username='admin_default'))
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, null=True)
+    pass
     
-    
-    
-
 class Student(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, default=CustomUser.objects.get(username='admin_default'))
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, null=True)
+    pass
